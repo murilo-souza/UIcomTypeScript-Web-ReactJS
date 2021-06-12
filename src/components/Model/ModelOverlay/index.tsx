863625fbbf7aef1a6d8dab57a726c5a11109c2e6
murilo-souza <murilo.l.souza232@gmail.com>
2021-06-12 19:12:27 -0300

    update

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import { useTransform } from 'framer-motion';
+import React, {useState, useCallback, useLayoutEffect} from 'react';
+import { CarModel } from '../ModelsContext';
+import useWrapperScroll from '../useWrapperScroll';
 
 import { Container } from './styles';
+interface Props{
+  model: CarModel
+}
 
+type SectionDimensions = Pick<HTMLDivElement, 'offsetTop' | 'offsetHeight'>
 
-const ModelOverlay: React.FC = ({ children }) => {
+const ModelOverlay: React.FC<Props> = ({ model , children }) => {
+  const {scrollY} = useWrapperScroll()
+
+  const getSectionDimensions = useCallback(() => {
+    return{
+      offsetTop: model.sectionRef.current?.offsetTop,
+      offsetHeight: model.sectionRef.current?.offsetHeight
+
+    } as SectionDimensions
+  }, [model.sectionRef])
+
+  const [dimensions, setDimensions] = useState<SectionDimensions>(
+    getSectionDimensions()
+  )
+
+  useLayoutEffect(()=>{
+    function onResize(){
+      window.requestAnimationFrame(() => setDimensions(getSectionDimensions()))
+    }
+
+    window.addEventListener('resize', onResize)
+
+    return() => window.removeEventListener('resize', onResize)
+  },[getSectionDimensions])
+
+  const sectionScrollProgress = useTransform(scrollY, y => (y-dimensions.offsetTop))
+
   return (
     <Container>
       {children}
